5a5585a95dae47cddfef3e410d8c225006c71ca6
Nafis-Sahriar <nafis.apid@gmail.com>
2026-06-03 08:25:39 +0600

    Recruiter Page desgin started - Implemented Use Session

--- src/app/dashboard/recruiter/page.jsx
+++ src/app/dashboard/recruiter/page.jsx
@@ -1,9 +1,25 @@
+"use client";
+import { authClient } from '@/lib/auth-client';
 import React from 'react';
 
 const RecruiterPage = () => {
+
+    const {data:session, isPending, refetch} = authClient.useSession();
+
+
+    if(isPending) {
+        return <div>Loading...</div>;
+    }
+
+    const userData = session?.user;
+
+    
+
+
+
     return (
         <div>
-            I Am A Recruiter
+            <h1 className="text-3xl font-bold mb-4">Welcome, {userData?.name}!</h1>
         </div>
     );
 };
