28fb1dfcdbc8559d69fc93953b279ce39981e093
Nafis-Sahriar <nafis.apid@gmail.com>
2026-06-09 23:41:51 +0800

    Application Table Created and also saved the app from crashing in unusual sign out

--- src/lib/core/session.js
+++ src/lib/core/session.js
@@ -16,8 +16,15 @@ export const getUserSession = async()=>{
 export const requireRole  = async (role)=>{
     const user = await getUserSession();
 
+    if(!user)
+    {
+        redirect('/signin');
+    }
+
     if(user.role !== role)
     {
         redirect('/unauthorized');
     }
+
+    return user;
 }
